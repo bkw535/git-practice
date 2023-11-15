da3ad1c63e60b5351c2487b12f7500a9c5e82bb9
GeonWoo Bae <bkw535@hansung.ac.kr>
2023-11-15 04:35:46 +0000

    add text Hello.java(2)

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world from GIT!");
         System.out.println("Changes!");
+        System.out.println("One more Changes!");
 	}
 }
 
